056f70b40a7d6030add0ef5e0414aa2f7e481cab
Under Penalty <underpenalty@gmail.com>
2025-07-22 22:51:20 -0400

    Starting implementation of basic API

--- src/main/java/org/robert/pollum/controller/MarkdownController.java
+++ src/main/java/org/robert/pollum/controller/MarkdownController.java
@@ -1,8 +1,14 @@
 package org.robert.pollum.controller;
 
-import java.util.UUID;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.robert.pollum.entity.MarkdownArticle;
+import org.robert.pollum.service.MarkdownService;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 
@@ -12,10 +18,30 @@ import jakarta.ws.rs.PathParam;
 @Path("markdown")
 public class MarkdownController {
 	
+	@Inject
+	MarkdownService markdownService;
+
 	@GET
 	@Path("/{id}")
-	public String getExistingMarkdownArticle(@PathParam("id") UUID id) {
-		return "###MARKDOWN";
+	public MarkdownArticle getExistingMarkdownArticle(@PathParam("id") Integer id) {
+		return markdownService.getMarkdownArticle(id);
+	}
+
+	@POST
+	public MarkdownArticle createMarkdownArticle(@RequestBody MarkdownArticle article) {
+		System.out.print(article.toString());
+		return markdownService.createMarkdownArticle(article);
+	}
+
+	@PUT
+	public MarkdownArticle upMarkdownArticle(@RequestBody MarkdownArticle article) {
+		return markdownService.updateMarkdownArticle(article);
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public Boolean deleteMarkdownArticle(@PathParam("id") Integer id) {
+		return markdownService.deleMarkdownArticle(id);
 	}
 	
 }
--- src/main/java/org/robert/pollum/service/MarkdownService.java
+++ src/main/java/org/robert/pollum/service/MarkdownService.java
@@ -14,4 +14,33 @@ public class MarkdownService {
 		optional.orElseThrow(() -> new NotFoundException(String.format("No Markdown Article found with ID: %s", id.toString())));
 		return optional.get();
 	}
+
+	public MarkdownArticle createMarkdownArticle(MarkdownArticle article) {
+		if(article.getId() != null) {
+			throw new IllegalArgumentException("No ID can be provided when creating a new Markdown Article");
+		}
+		validateMarkdownArticle(article);
+		article.persist();
+		return getMarkdownArticle(article.getId());
+	}
+
+	public MarkdownArticle updateMarkdownArticle(MarkdownArticle article) {
+		//TODO try with resources on updating an existing markdown article if it exists
+		// if it doesn't throw a NotFound exception. 
+		// try()
+		validateMarkdownArticle(article);
+		return new MarkdownArticle();
+	}
+
+	public Boolean deleMarkdownArticle(Integer id) {
+		if(MarkdownArticle.findByIdOptional(id).isEmpty()) {
+			throw new NotFoundException(String.format("No Markdown Article found with id: %d", id));
+		} 
+		return MarkdownArticle.deleteById(id);
+	}
+
+	private void validateMarkdownArticle(MarkdownArticle article) {
+		//TODO Implement validation logic in this funciton or do some of this through hibernate;
+		return;
+	}
 }
