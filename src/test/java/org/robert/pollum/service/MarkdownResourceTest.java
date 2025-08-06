5f9b8603265703542aea04ee9ba2bfd3f4a76209
Under Penalty <underpenalty@gmail.com>
2025-08-06 11:33:48 -0400

    Adding cleaned up test

--- src/test/java/org/robert/pollum/service/MarkdownResourceTest.java
+++ src/test/java/org/robert/pollum/service/MarkdownResourceTest.java
@@ -26,7 +26,7 @@ class MarkdownResourceTest {
           .when().post("/markdown"
           ).then()
           .statusCode(200)
-          .body(notNullValue());
+          .body("id", notNullValue());
     }
 
 }
